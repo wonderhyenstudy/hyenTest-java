546ae3a530837e2737e5bb6270c37da9033c37b1
wonderhyenstudy <wonderhyenstudy@gmail.com>
2026-01-13 11:26:05 +0900

    first commit

---
+++ java.Hello.World/src/_1_test_260113/_0_HelloWorld.java
@@ -0,0 +1,10 @@
+package _1_test_260113;
+
+public class _0_HelloWorld { //클래스
+    public static void main(String[] args) { //메서드
+        System.out.println("Hello World");
+        System.out.println("와와.잼있네"); //단축키:sout+enter
+    }
+}
+
+//자바 프로그램의 구조와 식별자
