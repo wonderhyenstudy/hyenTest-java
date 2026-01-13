6f37c5c24d2ca48b99110ab2670b370431a61d98
wonderhyenstudy <wonderhyenstudy@gmail.com>
2026-01-13 12:06:49 +0900

    오전수업

---
+++ java.Hello.World/src/_1_test_260113/_2_Test.java
@@ -0,0 +1,16 @@
+package _1_test_260113;
+
+public class _2_Test {
+    public static void main(String[] args) {
+//        1. 사용자 이름과 나이를 변수로 선언하고 출력하는 프로그램 작성
+            String name = "황혜은";
+            int age = 18;
+            System.out.println("이름 : " + name + ", 나이 : " + age);
+
+//        2. 두 수를 곱한 결과를 출력하는 프로그램. 변수, x = 100, y = 200
+            int x = 100;
+            int y = 200;
+            int result = x * y;
+            System.out.println("x*y = " + result);
+    }
+}
